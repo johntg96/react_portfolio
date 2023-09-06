f6d4ae67f300b1fccc37009fffb3dbc2f0c31a7b
John Garrison <splendidonkey@gmail.com>
2023-09-06 18:20:17 -0500

    reworded my bio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,14 +13,14 @@ export default function About() {
       <h3 className='mt-5 mb-4 d-flex justify-content-center align-items-center' id='about-me-title'>About Me</h3>
       <MDBContainer fluid className='d-flex flex-wrap justify-content-center about-me-content'>
         <MDBRow>
-          <MDBCol className='mt-4 d-flex justify-content-center'>
+          <MDBCol className='mt-4 mb-2 d-flex justify-content-center'>
             <img id='about-me-img' src='images/me.jpg'></img>
           </MDBCol>
           <MDBCol>
-            <MDBContainer id='about-me-text' className='mt-4 flex-wrap'>
+            <MDBContainer id='about-me-text' className='mt-4 mb-2 flex-wrap'>
               <p><em>Currently attending University of Minnesota Full-Stack Coding Bootcamp full-time.</em></p>
-              <p>I've been coding as a hobby for several years seeking opportunities to write code to provide solutions for problems I've encountered in my day-to-day life. I enjoy the essence of logical-creativity and am excited when I can leverage technology to engage users and drive productivity.</p>
-              <p>Through my recent endeavors I've expanded my knowledge base and additionally have experience working on several group development projects, including a full-stack React application <em style={{ fontSize: "80%" }}>(MERN)</em>.</p>
+              <p>I've been coding as a hobby for several years seeking opportunities to write code to provide solutions for problems I've encountered in my day-to-day life. I enjoy the essence of logical-creativity and find a lot of that within the processes of web development.</p>
+              <p>Through my computer endeavors I've participated several team-focused development projects, one being a full-stack react application <em style={{ fontSize: "80%" }}>(MERN stack)</em>.</p>
             </MDBContainer>
           </MDBCol>
         </MDBRow>
