d3ea52ed1884196e9585ed39885ad413ea0052c2
John Garrison <splendidonkey@gmail.com>
2023-09-15 17:10:10 -0500

    added latest project ("ThinkSync") to "Latest Projects" section. Added more project screenshots.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,7 +20,7 @@ export default function About() {
             <MDBContainer id='about-me-text' className='mt-4 mb-2 flex-wrap'>
               <p><em>Currently attending University of Minnesota Full-Stack Coding Bootcamp full-time.</em></p>
               <p>I've been coding as a hobby for several years seeking opportunities to write code to provide solutions for problems I've encountered in my day-to-day life. I enjoy the essence of logical-creativity and find a lot of that within the processes of web development.</p>
-              <p>Through my computer endeavors I've participated in several team-focused development projects, one being a full-stack react application <em style={{ fontSize: "80%" }}>(MERN stack)</em>.</p>
+              <p>Through my computer endeavors I've participated in several team-focused development projects, one being a full-stack react application <em style={{ fontSize: "80%" }}>(<a href="https://github.com/BenTrebi/ThinkSync" style={{ color: 'lightblue' }}>ThinkSync</a>)</em>.</p>
             </MDBContainer>
           </MDBCol>
         </MDBRow>
