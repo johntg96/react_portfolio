1589e2d1bf8aa5701150f57b3a109e84fb3a6566
John Garrison <splendidonkey@gmail.com>
2023-09-20 11:42:41 -0500

    added link to resume to about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,6 +21,7 @@ export default function About() {
               <p>I've been coding as a hobby for several years seeking opportunities to write code to provide solutions for problems I've encountered in my day-to-day life. I enjoy the essence of logical-creativity and find a lot of that within the processes of web development.</p>
               <p>I recently graduated from the University of Minnesota Full-Stack Flex Coding Bootcamp.</p>
               <p>Through my computer endeavors I've participated in several team-focused development projects, one being a full-stack react application <em style={{ fontSize: "80%" }}>(<a href="https://github.com/johntg96/ThinkSync" style={{ color: 'lightblue' }}>ThinkSync</a>)</em>.</p>
+              <p>My resume is available <a href='https://docs.google.com/document/d/15ecyRnBRVWjwhBGa8LjzBlZYNAOX3jnIuC4D3pmV-8o/edit?usp=sharing'>here</a>.</p>
             </MDBContainer>
           </MDBCol>
         </MDBRow>
