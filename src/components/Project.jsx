25d0b266aa888351b0c00fd46f5d6687d95935e9
John Garrison <splendidonkey@gmail.com>
2023-10-30 07:09:55 -0500

    added deployment links to visit live applications for a few projects and added github icon to title with link to corresponding projects repository

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import {  MDBContainer, 
+          MDBRow,
+          MDBCol,
+          MDBIcon
+} from 'mdb-react-ui-kit';
 
 export default function Project() {
 
@@ -11,7 +15,7 @@ export default function Project() {
       <MDBRow>
 
         <MDBCol id='project-1' className='project-item'>
-          <h4><a href='https://thinksync-1e5fbc55e5f7.herokuapp.com/'>ThinkSync <img src='/images/project-icons/thinksync.ico' style={{width: '1.5em'}}/></a></h4>
+          <h4><a href='https://thinksync-1e5fbc55e5f7.herokuapp.com/' title='visit deployed application'>ThinkSync <img src='/images/project-icons/thinksync.ico' style={{width: '1.5em'}}/></a>&nbsp;&nbsp;<a href='https://github.com/johntg96/ThinkSync' title='go to GitHub repository'><MDBIcon fab icon='github' /></a></h4>
           <p>ThinkSync is a web application for coming to a decision between multiple ideas. It is created with MongoDB, Express.js, React.js, Node.js (MERN stack) and Material Design for Bootstrap CSS framework. This is my final group project for University of Minnesota Full-Stack Web Development Coding Bootcamp.</p>
           <img src='images/screenshot_thinksync.png' className='project-screenshot'></img>
         </MDBCol>
@@ -20,19 +24,19 @@ export default function Project() {
       <MDBRow>
 
       <MDBCol id='project-2' className='project-item'>
-          <h4><a href='https://github.com/johntg96/konsoom'>konsoom&nbsp;&nbsp;<img src='images/project-icons/konsoom.ico' style={{width: '1.5em'}}/></a></h4>
+          <h4><a href='https://johntg96.github.io/konsoom/' title='visit deployed application'>konsoom&nbsp;&nbsp;<img src='images/project-icons/konsoom.ico' style={{width: '1.5em'}}/></a>&nbsp;&nbsp;<a href='https://github.com/johntg96/konsoom' title='go to GitHub repository'><MDBIcon fab icon='github' /></a></h4>
           <p>A collaborative project curating a movie and TV series list for future viewing, with cover art, dark mode, and persistent data storage using browser local storage. Data fetched through the OMDB API for comprehensive results.</p>
           <img src='images/screenshot_konsoom.png' className='project-screenshot'></img>
         </MDBCol>
 
         <MDBCol id='project-3' className='project-item'>
-          <h4><a href='https://github.com/johntg96/tech_blog'>tech_blog</a></h4>
+          <h4><a href='https://tech-blog-johntg96-6194751df56d.herokuapp.com/' title='visit deployed application'>tech_blog</a>&nbsp;&nbsp;<a href='https://github.com/johntg96/tech_blog' title='go to GitHub repository'><MDBIcon fab icon='github' /></a></h4>
           <p>Created using sequelize and express, this application has login/signup functionality and pages generated via Handlebars.</p>
           <img src='images/screenshot_tech_blog.png' className='project-screenshot'></img>
         </MDBCol>
 
         <MDBCol id='project-4' className='project-item'>
-          <h4><a href='https://github.com/johntg96/waterCalc'>waterCalc</a></h4>
+          <h4><a href='https://github.com/johntg96/waterCalc' title='go to GitHub Repository'>waterCalc</a></h4>
           <p>I developed a website to calculate Sodium Bicarbonate and Calcium Chloride weights for achieving optimal hot tub and pool water balance. Initially created for personal use during my role as a maintenance associate at the YMCA.</p>
           <img src='images/screenshot_waterCalc.png' className='project-screenshot'></img>
         </MDBCol>
@@ -41,7 +45,7 @@ export default function Project() {
       <MDBRow>
 
         <MDBCol id='project-5' className='project-item'>
-          <h4><a href='https://github.com/johntg96/h0ldb1nz'>h0ldb1nz</a></h4>
+          <h4><a href='https://github.com/johntg96/h0ldb1nz' title="go to GitHub repository">h0ldb1nz</a></h4>
           <p>I developed a specialized work tool that optimizes the logging of computer hardware and service repair order ticket status (SRO) through a graphical user interface. This tool significantly enhanced my efficiency in performing specific work tasks. Written in vanilla HTML, CSS, and JavaScript then wrapped as a desktop application via Electron. Data is read and saved using local JSON files.</p>
           <img src='images/screenshot_h0ldb1nz.png' className='project-screenshot'></img>
         </MDBCol>
