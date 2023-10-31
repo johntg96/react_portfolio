2c55a63e5da87e5e41503fce27fd4ca4bb0a9b41
John Garrison <splendidonkey@gmail.com>
2023-10-31 12:53:01 -0500

    another small adjustment to about-me section content placement

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,11 +14,11 @@ export default function About() {
       <MDBContainer fluid className='about-me-content'>
         <MDBRow>
           <MDBCol>
-              <div className='mt-4 mb-2 d-flex justify-content-center align-items-center'>
+              <div className='mt-4 mb-2 d-flex justify-content-center align-items-center' >
                 <img id='about-me-img' src='images/me_animated.gif'></img>
               </div>
           </MDBCol>
-          <MDBCol className='d-flex flex-wrap justify-content-start align-items-center'>
+          <MDBCol className='mt-4'>
             <ul>
               <li><p>Through my computer endeavors I've participated in several team-focused development projects, one being a full-stack react application <em style={{ fontSize: "80%" }}>(<a href="https://github.com/johntg96/ThinkSync" style={{ color: 'lightblue' }}>ThinkSync</a>)</em>.</p></li>
               <li><p>My resume is available &#x2192; <a href='https://docs.google.com/document/d/1sd07dV3R7xevumimU5AJD6_v1yE2rwYursuW1r_Ypqg/edit?usp=sharing' id='resume-link' style={{textDecoration:'underline'}}>here</a><span style={{fontSize: '0.7em', marginLeft:'1em'}}><br/>(Google Docs)</span></p></li>
