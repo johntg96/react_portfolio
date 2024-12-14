78a32e055aff0cda09de9a71ab23ff15a7e700f8
johntg96 <git@caret.mailer.me>
2024-12-13 21:06:26 -0500

    renamed back to previous

--- src/components/email.jsx
+++ src/components/email.jsx
@@ -2,12 +2,12 @@ import emailjs from "@emailjs/browser";
 
 // This uses emailjs service to send an email
 const sendCustomEmail = (details) => {
-    
+
     // initialize using User ID from .env file
-    emailjs.init(import.meta.env.EMAIL_USER_ID);
+    emailjs.init(import.meta.env.VITE_EMAIL_USER_ID);
     emailjs.send(
-        import.meta.env.EMAIL_SERVICE_ID,
-        import.meta.env.EMAIL_TEMPLATE_ID,
+        import.meta.env.VITE_EMAIL_SERVICE_ID,
+        import.meta.env.VITE_EMAIL_TEMPLATE_ID,
 
         {
             from_name: details.name,
